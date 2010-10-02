e1fe4e41d6beacdb4553510961e3de08d4cc6f9f
tsteur <thomas.steur@googlemail.com>
2010-10-02 18:20:01 +0000

    use native date picker on android
    git-svn-id: http://dev.piwik.org/svn/mobile/trunk@3206 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- Resources/library/Ui/Picker.js
+++ Resources/library/Ui/Picker.js
@@ -18,44 +18,12 @@
  */
 function Ui_Picker (params) {
     
-    this.maxDate = new Date();
-    
-    this.minDate = new Date(2008, 0, 1);
-    
-    this.value   = new Date();
-    
     this.selectionIndicator = false;
     
     this.period  = 'day';
 
     this.create = function (params) {
-        
-        if (!params) {
-            params = {};
-        }
     
-        if (params.maxDate) {
-            this.maxDate = params.maxDate;
-        }
-        
-        if (params.period) {
-            this.period = params.period;
-        }
-        
-        this.maxDate.setHours(0);
-        this.maxDate.setMilliseconds(0);
-        this.maxDate.setMinutes(0);
-        this.maxDate.setSeconds(0);
-        
-        if (params.minDate) {
-            this.minDate = params.minDate;
-        }
-        
-        this.minDate.setHours(0);
-        this.minDate.setMilliseconds(0);
-        this.minDate.setMinutes(0);
-        this.minDate.setSeconds(0);
-        
         if (params.value) {
             this.value = params.value;
         }
@@ -63,7 +31,7 @@ function Ui_Picker (params) {
         var view = this.createView();
         
         this.addTitle(view);
-        this.addDateSelector(view);
+        this.addDateSelector(view, params);
         this.addButtons(view);
         
         this.setValue(this.value);
@@ -82,9 +50,9 @@ function Ui_Picker (params) {
                                               borderWidth: 2,
                                               borderColor: '#ffffff'});
         
-        view.height = 207;
+        view.height = 230;
         view.top    = 50;
-        view.width  = 200;
+        view.width  = 275;
         view.zIndex = 99;
         
         return view;
@@ -97,8 +65,8 @@ function Ui_Picker (params) {
             height: 32,
             left: 10,
             top: 5,
-            right: 10,
             color: '#efefef',
+            width: 240,
             font: {fontSize: 14, fontFamily: config.theme.fontFamily}
         });
         
@@ -109,270 +77,30 @@ function Ui_Picker (params) {
     this.updateDisplayedValues = function () {
     
         this.titleLabel.text = this.value.toPiwikDateRangeString(this.period);
-        
-        this.dayValue.text   = '' + this.value.getDate();
-            
-        this.monthValue.text = '' + (this.value.getMonth() + 1);
-            
-        this.yearValue.text  = '' + (this.value.getYear() + 1900);
-        
-        var activeBgColor    = '#f3f3f3';
-        var inactiveBgColor  = '#d4d4d4';
-        
-        // simulate each possible click
-        
-        var testMaxYear = this.value;
-        testMaxYear.setYear(testMaxYear.getYear() + 1 + 1900);
-        
-        if (testMaxYear > this.maxDate) {
-            this.yearValueAdd.backgroundColor  = inactiveBgColor;
-        } else {
-            this.yearValueAdd.backgroundColor  = activeBgColor;
-        }
-        
-        var testMinYear = this.value;
-        testMinYear.setYear(testMinYear.getYear() - 1 + 1900);
-        
-        if (testMinYear < this.minDate) {
-            this.yearValueSub.backgroundColor  = inactiveBgColor;
-        } else {
-            this.yearValueSub.backgroundColor  = activeBgColor;
-        }
-        
-        var testMaxMonth = this.value;
-        testMaxMonth.setMonth(testMaxMonth.getMonth() + 1);
-        
-        if (testMaxMonth > this.maxDate) {
-            this.monthValueAdd.backgroundColor = inactiveBgColor;
-        } else {
-            this.monthValueAdd.backgroundColor = activeBgColor;
-        }
-        
-        var testMinMonth = this.value;
-        testMinMonth.setMonth(testMinMonth.getMonth() - 1);
-        
-        if (testMinMonth < this.minDate) {
-            this.monthValueSub.backgroundColor = inactiveBgColor;
-        } else {
-            this.monthValueSub.backgroundColor = activeBgColor;
-        }
-        
-        var testMaxDay = this.value;
-        testMaxDay.setDate(testMaxDay.getDate() + 1);
-        
-        if (testMaxDay > this.maxDate) {
-            this.dayValueAdd.backgroundColor   = inactiveBgColor;
-        } else {
-            this.dayValueAdd.backgroundColor   = activeBgColor;
-        }
-        
-        var testMinDay = this.value;
-        testMinDay.setDate(testMinDay.getDate() - 1);
-        
-        if (testMinDay < this.minDate) {
-            this.dayValueSub.backgroundColor   = inactiveBgColor;
-        } else {
-            this.dayValueSub.backgroundColor   = activeBgColor;
-        }
     };
     
     this.setValue = function (value) {
+    
+        this.value = value;
         
-        value.setHours(0);
-        value.setMilliseconds(0);
-        value.setMinutes(0);
-        value.setSeconds(0);
+        var event  = {value: this.value};
         
-        if (value >= this.minDate && value <= this.maxDate) {
-        
-            this.value = value;
-            
-            var event  = {value: this.value};
-            
-            this.fireEvent('change', event);
-            
-            this.updateDisplayedValues();
-        }
+        this.updateDisplayedValues();
     };
 
-    this.addDateSelector = function (view) {
+    this.addDateSelector = function (view, params) {
         
-        var _this     = this;
+        var _this    = this;
+        params.top   = 45;
+        params.left  = 7;
         
-        this.dayValue =  Titanium.UI.createLabel({
-            text: '',
-            left: 10,
-            top: 82,
-            width: 50,
-            color: '#333333',
-            height: 35,
-            textAlign: 'center',
-            backgroundColor: '#d4d4d4',
-            borderColor: '#7a7a7a',
-            borderWidth: 1,
-            font: {fontSize: 22, fontWeight: 'bold', fontFamily: config.theme.fontFamily},
-            zIndex: 3
+        var picker = Titanium.UI.createPicker(params);
+        
+        picker.addEventListener('change', function (event) {
+            _this.setValue(event.value);
         });
         
-        this.monthValue =  Titanium.UI.createLabel({
-            text: '',
-            left: 65,
-            top: 82,
-            width: 50,
-            color: '#333333',
-            height: 35,
-            textAlign: 'center',
-            backgroundColor: '#d4d4d4',
-            borderColor: '#7a7a7a',
-            borderWidth: 1,
-            font: {fontSize: 22, fontWeight: 'bold', fontFamily: config.theme.fontFamily},
-            zIndex: 3
-        });
-        
-        this.yearValue =  Titanium.UI.createLabel({
-            text: '',
-            left: 120,
-            top: 82,
-            width: 70,
-            color: '#333333',
-            height: 35,
-            textAlign: 'center',
-            backgroundColor: '#d4d4d4',
-            borderColor: '#7a7a7a',
-            borderWidth: 1,
-            font: {fontSize: 22, fontWeight: 'bold', fontFamily: config.theme.fontFamily},
-            zIndex: 3
-        });
-
-        this.dayValueAdd =  Titanium.UI.createButton({
-            title: '+',
-            left: 10,
-            top: 52,
-            width: 50,
-            height: 30,
-            color: '#7e7e7e',
-            borderColor: '#333333',
-            borderWidth: 1,
-            font: {fontSize: 20, fontWeight: 'bold', fontFamily: config.theme.fontFamily}
-        });
-        
-        this.dayValueAdd.addEventListener('click', function () {
-
-            var value = _this.value;
-            value.setDate(value.getDate() + 1);
-            
-            _this.setValue(value);
-        });
-        
-        this.monthValueAdd =  Titanium.UI.createButton({
-            title: '+',
-            left: 65,
-            top: 52,
-            width: 50,
-            height: 30,
-            color: '#7e7e7e',
-            borderColor: '#333333',
-            borderWidth: 1,
-            font: {fontSize: 20, fontWeight: 'bold', fontFamily: config.theme.fontFamily}
-        });
-        
-        this.monthValueAdd.addEventListener('click', function () {
-
-            var value = _this.value;
-            value.setMonth(value.getMonth() + 1);
-            
-            _this.setValue(value);
-        });
-        
-        this.yearValueAdd =  Titanium.UI.createButton({
-            title: '+',
-            left: 120,
-            top: 52,
-            width: 70,
-            height: 30,
-            color: '#7e7e7e',
-            borderColor: '#333333',
-            borderWidth: 1,
-            font: {fontSize: 20, fontWeight: 'bold', fontFamily: config.theme.fontFamily}
-        });
-        
-        this.yearValueAdd.addEventListener('click', function () {
-
-            var value = _this.value;
-            value.setYear(value.getYear() + 1 + 1900);
-            
-            _this.setValue(value);
-        });
-
-        this.dayValueSub =  Titanium.UI.createButton({
-            title: '-',
-            left: 10,
-            top: 117,
-            width: 50,
-            height: 30,
-            color: '#7e7e7e',
-            borderColor: '#333333',
-            borderWidth: 1,
-            font: {fontSize: 20, fontWeight: 'bold', fontFamily: config.theme.fontFamily}
-        });
-        
-        this.dayValueSub.addEventListener('click', function () {
-
-            var value = _this.value;
-            value.setDate(value.getDate() - 1);
-            
-            _this.setValue(value);
-        });
-        
-        this.monthValueSub =  Titanium.UI.createButton({
-            title: '-',
-            left: 65,
-            top: 117,
-            width: 50,
-            height: 30,
-            color: '#7e7e7e',
-            borderColor: '#333333',
-            borderWidth: 1,
-            font: {fontSize: 20, fontWeight: 'bold', fontFamily: config.theme.fontFamily}
-        });
-        
-        this.monthValueSub.addEventListener('click', function () {
-
-            var value = _this.value;
-            value.setMonth(value.getMonth() - 1);
-            
-            _this.setValue(value);
-        });
-        
-        this.yearValueSub =  Titanium.UI.createButton({
-            title: '-',
-            left: 120,
-            top: 117,
-            width: 70,
-            height: 30,
-            color: '#7e7e7e',
-            borderColor: '#333333',
-            borderWidth: 1,
-            font: {fontSize: 20, fontWeight: 'bold', fontFamily: config.theme.fontFamily}
-        });
-        
-        this.yearValueSub.addEventListener('click', function () {
-
-            var value = _this.value;
-            value.setYear(value.getYear() - 1 + 1900);
-            
-            _this.setValue(value);
-        });
-
-        view.add(this.dayValueAdd);
-        view.add(this.monthValueAdd);
-        view.add(this.yearValueAdd);
-        view.add(this.dayValue);
-        view.add(this.monthValue);
-        view.add(this.yearValue);
-        view.add(this.dayValueSub);
-        view.add(this.monthValueSub);
-        view.add(this.yearValueSub);
+        view.add(picker);
     };
     
     this.addButtons = function (view) {
@@ -389,7 +117,7 @@ function Ui_Picker (params) {
             title: _('CoreUpdater_UpdateTitle'),
             left: 10,
             top: 5,
-            width: 70,
+            width: 120,
             height: 30,
             color: '#333333',
             backgroundColor: '#f3f3f3',
@@ -410,8 +138,7 @@ function Ui_Picker (params) {
         
         this.cancelButton =  Titanium.UI.createButton({
             title: _('SitesManager_Cancel_js'),
-            left: 90,
-            width: 60,
+            width: 120,
             top: 5,
             right: 10,
             height: 30,
@@ -486,34 +213,51 @@ function create_Ui_Picker(params) {
         
         Titanium.UI.currentWindow.add(picker);
 
-        var buttonBar = Titanium.UI.createButtonBar({labels:[_('CoreUpdater_UpdateTitle'), _('SitesManager_Cancel_js')],
-                                                     backgroundColor:'#336699',
-                                                     bottom:picker.height,
-                                                     left: 0,
-                                                     right: 0,
-                                                     style:Titanium.UI.iPhone.SystemButtonStyle.BAR,
-                                                     height:25});
+        var update = Titanium.UI.createButton({
+            title:_('CoreUpdater_UpdateTitle'),
+            style:Titanium.UI.iPhone.SystemButtonStyle.DONE
+        });
+
+        var cancel = Titanium.UI.createButton({
+            title:_('SitesManager_Cancel_js'),
+            style:Titanium.UI.iPhone.SystemButtonStyle.BORDERED                
+        });
+       
+        var flexSpace = Titanium.UI.createButton({
+            systemButton:Titanium.UI.iPhone.SystemButton.FLEXIBLE_SPACE
+        });
+
+        var toolbar = Titanium.UI.createToolbar({
+            items:[flexSpace,update, cancel,flexSpace],
+            bottom:0,
+            borderTop:true,
+            borderBottom:false,
+            translucent:true,
+            barColor:'#999'
+        });
         
-        buttonBar.pickerValue = params.value;
+        Titanium.UI.currentWindow.add(toolbar);
+
+        update.pickerValue = params.value;
         
         picker.addEventListener('change', function (event) {
-               buttonBar.pickerValue = event.value;
+            update.pickerValue = event.value;
         });
         
-        buttonBar.addEventListener('click', function (event) {
-            if (0 == event.index) {
-                
-                var event = {value: buttonBar.pickerValue};
-                
-                picker.fireEvent('set', event);
-            } 
-            
-            buttonBar.hide();
+        cancel.addEventListener('click', function (event) {
+            toolbar.hide();
             Titanium.UI.currentWindow.remove(picker);
-            Titanium.UI.currentWindow.remove(buttonBar);
+            Titanium.UI.currentWindow.remove(toolbar);
         });
         
-        Titanium.UI.currentWindow.add(buttonBar);
+        update.addEventListener('click', function (event) {
+            var event = {value: update.pickerValue};
+                           
+            toolbar.hide();
+            picker.fireEvent('set', event); 
+            Titanium.UI.currentWindow.remove(picker);
+            Titanium.UI.currentWindow.remove(toolbar);
+        });
             
         return picker;
     }
