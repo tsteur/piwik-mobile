de490eadf4beb6b11e0b7cbdb0d277551c9a1323
tsteur <thomas.steur@googlemail.com>
2010-11-01 21:52:23 +0000

    update prepare release script
    git-svn-id: http://dev.piwik.org/svn/mobile/trunk@3282 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- tools/preparerelease.sh
+++ tools/preparerelease.sh
@@ -6,24 +6,35 @@
 #
 # Link: http://piwik.org
 # License: http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
+
+files=$(sed -n "s/Titanium\.include('\(.*\).*');/\1/p" ../Resources/library/all.js)
+
+echo "shrink js files" 
  
 for I in `find ../Resources/ -name "*.js" -type f` ; 
 
  do echo $I;
- java -jar closurecompiler/compiler.jar  --compilation_level SIMPLE_OPTIMIZATIONS --charset utf-8 --define='Log.ENABLED=false' --js $I --js_output_file $I"_minify.js"
+ java -jar closurecompiler/compiler.jar  --compilation_level SIMPLE_OPTIMIZATIONS --logging_level=1000 --third_party=true --warning_level=quiet --charset utf-8 --define='Log.ENABLED=false' --js $I --js_output_file $I"_minify.js"
+ ## --formatting=pretty_print --summary_detail_level 1
  rm $I;
  mv $I"_minify.js" $I;
 
 done
 
+echo "merge following files into all.js"
+
 echo '' > ../Resources/library/all.js;
 
-for I in `find ../Resources/library -name "*[!all].js" -type f` ; 
+for file in $files
+do 
+echo $file
+cat ../Resources$file >> ../Resources/library/all.js
+done 
 
- do cat $I >> ../Resources/library/all.js
+echo "removing wrong app logo if exists"
 
-done
-
-# TODO delete Resources/default_app_logo.png if exists
-
-# TODO remove android/iphone specific files depending on a parameter
+if [ -f ../Resources/default_app_logo.png ];
+then
+echo "wrong app logo found and deleted"
+rm -f ../Resources/default_app_logo.png
+fi
