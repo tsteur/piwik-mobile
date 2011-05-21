5eb50450ad91c1943621e285ce34deee06f72a06
tsteur <thomas.steur@googlemail.com>
2011-05-21 13:32:26 +0000

    require doc output path as an argument
    git-svn-id: http://dev.piwik.org/svn/mobile/trunk@4747 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- tools/createdoc.sh
+++ tools/createdoc.sh
@@ -5,4 +5,13 @@
 # Link: http://piwik.org
 # License: http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
 
-java -jar jsdoc-toolkit/jsrun.jar jsdoc-toolkit/app/run.js -r=5 ../Resources -t=jsdoc-toolkit/templates/jsdoc -p -d=../docs --encoding=UTF-8
+if [ $# -ne 1 ]
+then
+    echo "Not correct number of parameters."
+    echo "Usage: createdoc.sh <doc_output_path>"
+    exit 1
+fi
+
+DOC_OUTPUT_PATH=$1
+
+java -jar jsdoc-toolkit/jsrun.jar jsdoc-toolkit/app/run.js -r=5 ../Resources -t=jsdoc-toolkit/templates/jsdoc -p -d=$DOC_OUTPUT_PATH --encoding=UTF-8
