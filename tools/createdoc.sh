407d00f3863ae613cee59b925ad7fe414697ab82
tsteur <thomas.steur@googlemail.com>
2011-05-21 12:54:55 +0000

    deleting the whole trunk dir for an upcoming new code base (Piwik Mobile)
    git-svn-id: http://dev.piwik.org/svn/mobile/trunk@4745 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- tools/createdoc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Generates the developer api documentation using jsdoc toolkit.
-#
-# Link: http://piwik.org
-# License: http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
-
-java -jar jsdoc-toolkit/jsrun.jar jsdoc-toolkit/app/run.js -r=5 ../Resources -t=jsdoc-toolkit/templates/jsdoc -p -d=../docs --encoding=UTF-8
