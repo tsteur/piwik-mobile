349ac47f01979bfcc76e1c18f52f80e2f039ed17
tsteur <thomas.steur@googlemail.com>
2011-06-07 17:16:17 +0000

    update default translation
    git-svn-id: http://dev.piwik.org/svn/mobile/trunk@4889 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- Resources/library/Piwik/Locale/Translation.js
+++ Resources/library/Piwik/Locale/Translation.js
@@ -165,7 +165,7 @@ Piwik.Locale.Translation = new function () {
         Mobile_ChooseHttpTimeout:                                           'Choose HTTP timeout value',
         Mobile_DefaultReportDate:                                           'Report date',
         Mobile_EnableGraphsLabel:                                           'Display graphs',
-        Mobile_HttpIsNotSecureWarning:                                      "Your Piwik authToken is sent in clear text if you use 'HTTP'. For this reason we recommend HTTPS for secure transport of data over the internet. Do you want to proceed?",
+        Mobile_HttpIsNotSecureWarning:                                      "Your Piwik authorization token (token_auth) is sent in clear text if you use 'HTTP'. For this reason we recommend HTTPS for secure transport of data over the internet. Do you want to proceed?",
         Mobile_LastUpdated:                                                 'Last Updated: %s',
         Mobile_MultiChartLabel:                                             'Display sparklines',
         Mobile_MultiChartInfo:                                              'Next to each website on the welcome screen',
