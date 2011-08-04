0b418c9d5c2cd77db053eb92f86372c79a36b7d8
tsteur <thomas.steur@googlemail.com>
2011-08-04 17:28:28 +0000

    fixes #2553 forgot to add new iphone layout
    git-svn-id: http://dev.piwik.org/svn/mobile/trunk@5078 59fd770c-687e-43c8-a1e3-f5a4ff64c105

---
+++ Resources/windows/layout/iphone.js
@@ -0,0 +1,179 @@
+/**
+ * Piwik - Web Analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
+ * @version $Id$
+ *
+ * @fileOverview layout 'layout/iphone.js' .
+ */
+
+/**
+ * @class Piwik Mobile iPhone layout. Handles how header, menu and the content will be displayed. How new windows 
+ *        will be removed or added and so on.
+ *
+ * @this {Piwik.UI.Window}
+ */
+function window () {
+
+    /**
+     * An array holding all current opened windows. Each new created window will be pushed to this array.
+     * On the contrary we have to pop a window from this array as soon as we close/remove it.
+     *
+     * @type Array
+     */
+    this.windows   = [];
+    
+    /**
+     * zIndex counter. Will be increased by one for each new created window. This ensures a new created window will be
+     * displayed in front of another window.
+     *
+     * @default "0"
+     *
+     * @type Number
+     */
+    this.zIndex    = 0;
+
+    /**
+     * A reference to the layout header.
+     *
+     * @default null
+     *
+     * @type Piwik.UI.Header
+     */
+    this.header    = null;
+
+    /**
+     * A reference to the layout menu.
+     *
+     * @default null
+     *
+     * @type Piwik.UI.Menu
+     */
+    this.menu      = null;
+
+    /**
+     * @private
+     */
+    var layout     = this;
+
+    /**
+     * @private
+     */
+    var rootWindow = Ti.UI.currentWindow;
+    
+    /**
+     * Retrieve the current displayed window.
+     *
+     * @returns {void|Piwik.UI.Window}  The current displayed window or void if no window is opened.
+     * 
+     * @private
+     */
+    this._getCurrentWindow = function () {
+        if (this.windows && this.windows.length) {
+
+            return this.windows[this.windows.length - 1];
+        }
+    };
+
+    /**
+     * Add a new window to the layout so that it will be visible afterwards. Does also add a property named
+     * "rootWindow" to the newWin object which references to the root window (Ti.UI.Window) of the view.
+     *
+     * @param    {Piwik.UI.Window}    newWin   The window that shall be added to the layout.
+     *                                         The window will be visible afterwards.
+     */
+    this.addWindow = function (newWin) {
+
+        var currentWindow = this._getCurrentWindow();
+        if (currentWindow) {
+            currentWindow.fireEvent('blurWindow', {});
+        }
+
+        var win = Ti.UI.createWindow({className: 'piwikRootWindow', backButtonTitle: _('Mobile_NavigationBack')});
+        win.open();
+        
+        win.add(newWin);
+        win.addEventListener('close', function () {
+            newWin.close();
+        });
+
+        if (!this.navigation) {
+                
+            this.navigation = Ti.UI.iPhone.createNavigationGroup({window: win});
+            rootWindow.add(this.navigation);
+            
+        } else {
+            this.navigation.open(win);
+        }
+
+        newWin.rootWindow      = win;
+
+        Piwik.UI.currentWindow = newWin;
+        
+        newWin.fireEvent('focusWindow', {});
+
+        this.windows.push(newWin);
+    };
+
+    /**
+     * Remove a window from the layout so that it will be no longer visible. If no new window follows and the
+     * window that shall be removed is the only displayed window, we'll not remove the window on iOS and we exit the
+     * application on Android respectively in such a case.
+     *
+     * @param    {Piwik.UI.Window}  window                The window that shall be removed from the layout. The
+     *                                                    current implementation requires that the given window,
+     *                                                    which shall be removed, is the current displayed window.
+     * @param    {boolean}          newWindowWillFollow   True if a new window will follow afterwards (via addWindow),
+     *                                                    false otherwise. This is important cause it has an impact on
+     *                                                    how to remove the window.
+     */
+    this.removeWindow = function (window, newWindowWillFollow) {
+        if (!window || window.alreadyClosed) {
+            return;
+        }
+        window.alreadyClosed = true;
+        
+        // window should be equal to current displayed window
+        var oldWindow = this.windows.pop();
+
+        try {
+
+            this.navigation.close(window.rootWindow);
+
+        } catch (e) {
+            Piwik.Log.warn('Failed to remove window from navigation ' + e, 'iPhoneLayout::removeWindow');
+        }
+
+        var newActiveWindow = this._getCurrentWindow();
+
+        if (!newActiveWindow) {
+
+            return;
+        }
+
+        Piwik.UI.currentWindow = newActiveWindow;
+        newActiveWindow.fireEvent('focusWindow', {});
+
+        // bring previous displayed window to front if no new window follows
+        if (!newWindowWillFollow) {
+
+            /**
+             * we do not reset zIndex if a new window will follow cause zIndex is already correct
+             * in such a case
+             */
+            this.zIndex = Piwik.UI.currentWindow.zIndex;
+        }
+    };
+
+    /**
+     * Initialize the layout.
+     *
+     * Renders the basic layout like header and menu. This method should be executed only once during app session.
+     */
+    this.init = function () {
+
+        this.header = Piwik.UI.createHeader({title: 'Piwik Mobile'});
+        this.menu   = Piwik.UI.createMenu({menuView: this.header.getHeaderView()});
+    };
+}
